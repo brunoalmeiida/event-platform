aaf812d39880af6cd303e4028a7374e6706374da
Bruno Almeida <brunoalmeidasantos93@gmail.com>
2023-05-31 15:37:49 -0300

    Roteamento e player da aula

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.hygraph.com/v2/cliaknrpk09cj01t9hh2i4tmr/master",
-  cache: new InMemoryCache()
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
+  cache: new InMemoryCache(),
 })
